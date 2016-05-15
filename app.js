54a64050185558f1c5f28a1ccafdf09eaafd29a5
Vinay <vinay.hegde@dreamorbit.com>
2016-05-15 18:30:55 +0530

    Adding Packages

--- app.js
+++ app.js
@@ -4,15 +4,15 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
-
+  , routes = require('./routes'),
+  handlebars = require('handlebars');
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'handlebars');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
